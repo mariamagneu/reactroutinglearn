1fca1864d044cdeadff850e1f705591843f15afb
maria magdalena <neumariamagdalena@gmail.com>
2024-06-27 11:18:19 +0200

    make navbar links NavLinks & add styling to them in css

--- react-routing-intro/src/App.jsx
+++ react-routing-intro/src/App.jsx
@@ -1,8 +1,29 @@
-// App.jsx
-
 import "./App.css";
+// App.jsx
+// ... previous imports stay unchanged
+
+import Navbar from "./components/Navbar"; // <== IMPORT
+import HomePage from "./pages/Homepage"; // <== IMPORT
+import AboutPage from "./pages/AboutPage"; // <== IMPORT
+import ProjectsPage from "./pages/ProjectsPage"; // <== IMPORT
+import ErrorPage from "./pages/ErrorPage"; // <== IMPORT
+
+import { Routes, Route } from "react-router-dom"; // <== IMPORT
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+
+      {/*   Add <Route /> components between <Routes> and </Routes>   */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </div>
+  );
 }
+
 export default App;
---
+++ react-routing-intro/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+// src/components/Navbar.jsx
+
+import { Link, NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="Navbar">
+      <ul>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "selected" : "")}
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "selected" : "")}
+        >
+          About
+        </NavLink>
+
+        <NavLink
+          to="/projects"
+          className={({ isActive }) => (isActive ? "selected" : "")}
+        >
+          Projects
+        </NavLink>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
